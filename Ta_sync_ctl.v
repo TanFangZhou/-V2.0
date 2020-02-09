f949d749df908ac71b6bba82de3cb67af7cd4b74
TanFangZhou <1461773209@qq.com>
2020-02-09 11:51:45 +0800

    新建工程以及.v文件

---
+++ Ta_sync_ctl.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/02/09 11:45:27
+// Design Name: 
+// Module Name: Ta_sync_ctl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Ta_sync_ctl(
+
+    );
+endmodule
