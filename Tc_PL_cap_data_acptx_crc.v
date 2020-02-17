3f3e09c2c13be86070b665a16cc3da02ec4913f6
TanFangZhou <1461773209@qq.com>
2020-02-17 16:23:18 +0800

    开始整理调试

--- Tc_PL_cap_data_acptx_crc.v
+++ Tc_PL_cap_data_acptx_crc.v
@@ -32,11 +32,29 @@ input      [63:0]         crc_data        ,
 input                     crc_data_valid
     );
 
+reg[31:0] t_crc32=0;
 
+wire[31:0] crc_out;
+Tcrc32_64
+Tcrc32_64_ins0
+(
+.clk         (clk         ),
+.rst         (!crc_en     ),
+.data        (crc_data        ),
+.data_valid  (crc_data_valid  ),
+.crc_out     (crc_out     )
+    );
 
+always@(posedge clk)begin
+	if(rst)begin
+		t_crc32 <= 0;
+	end else begin
+		if(crc_en)begin
+			t_crc32 <= crc_out;
+		end
+	end
+end
 
-
-
-
+assign crc32 = t_crc32;
 
 endmodule
